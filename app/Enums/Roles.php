78325e380ba13fa9ce600d454f4a4878eef0aac6
Kyle Mabaso <kyle@insytive.co.za>
2022-02-15 13:48:08 +0200

    Updated Roles

---
+++ app/Enums/Roles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+abstract class Roles
+{
+    const ADMIN = 'Admin';
+    const INSTRUCTOR = 'Instructor';
+    const FACILITATOR = 'Facilitator';
+    const USER = 'User';
+}
